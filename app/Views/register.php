c33e7e70810872ee57b3edc749534be9ab430a0b
MAZIHBouchra <mazihbouchra@gmail.com>
2024-12-27 16:26:13 +0100

    register first commit

---
+++ app/Views/register.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+    <link href="assets/css/style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div class ="row">
+	<div class="col-md-6 mx-auto p-0">
+		<div class="card">
+<div class="login-box">
+	<div class="login-snip">
+		<input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">Login</label>
+		<input id="tab-2" type="radio" name="tab" class="sign-up"><label for="tab-2" class="tab">Sign Up</label>
+		<div class="login-space">
+			<div class="sign-up-form">
+				<div class="group">
+					<label for="user" class="label">Username</label>
+					<input id="user" name="Username" type="text" class="input" placeholder="Create your Username" >
+				</div>
+				<div class="group">
+					<label for="pass" class="label">Password</label>
+					<input id="pass" name="Password" type="password" class="input" data-type="password" placeholder="Create your password">
+				</div>
+				<div class="group">
+					<label for="pass" class="label">Repeat Password</label>
+					<input id="pass" name="Password_" type="password" class="input" data-type="password" placeholder="Repeat your password" >
+				</div>
+				<div class="group">
+					<label for="pass" class="label">Email Address</label>
+					<input id="pass" name="Email" type="text" class="input" placeholder="Enter your email address">
+				</div>
+				<div class="group">
+					<input type="submit" class="button" value="Sign Up">
+				</div>
+				<div class="hr"></div>
+				<div class="foot">
+					<label for="tab-1">Already Member?</label>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>   
+</div>
+</div>
+</div>
+
+</body>
+</html>
