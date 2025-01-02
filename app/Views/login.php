f6ae76e2c93333861de2b5da2a28725198c453d3
MAZIHBouchra <mazihbouchra@gmail.com>
2025-01-02 23:24:30 +0100

    Update application: add home page/update login.php

--- app/Views/login.php
+++ app/Views/login.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="http://localhost/codeigniter4-projets/public/assets/css/style.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/style.css" rel="stylesheet" type="text/css">
 </head>
 <body>
 	<div class="row">
@@ -13,7 +13,7 @@
 				<div class="login-box">
 					<div class="login-snip">
 						<input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">Login</label>
-						<input id="tab-2" type="radio" name="tab" class="sign-up"><label for="tab-2" class="tab">Sign Up</label>
+						<input id="tab-2" type="radio" name="tab" class="sign-up"><label for="tab-2" class="tab"><a href="register.php">Sign Up</a></label>
 						<div class="login-space">
 							<div class="login">
 								<form action="/process_login" method="post">
@@ -39,7 +39,31 @@
 								</form>
 							</div>
 							<div class="sign-up-form">
-								<!-- Placez ici le formulaire "Sign Up" si nécessaire -->
+							<form action="/process_register" method="post">
+                                <div class="group">
+                                    <label for="username" class="label">Username</label>
+                                    <input id="username" type="text" class="input" placeholder="Create your Username" name="Username" required>
+                                </div>
+                                <div class="group">
+                                    <label for="password" class="label">Password</label>
+                                    <input id="password" type="password" class="input" data-type="password" placeholder="Create your password" name="Password" required>
+                                </div>
+                                <div class="group">
+                                    <label for="password_repeat" class="label">Repeat Password</label>
+                                    <input id="password_repeat" type="password" class="input" data-type="password" placeholder="Repeat your password" name="Password_repeat" required>
+                                </div>
+                                <div class="group">
+                                    <label for="email" class="label">Email Address</label>
+                                    <input id="email" type="email" class="input" placeholder="Enter your email address" name="Email" required>
+                                </div>
+                                <div class="group">
+                                    <input type="submit" class="button" name="submit" value="Sign Up">
+                                </div>
+                                <div class="hr"></div>
+                                <div class="foot">
+                                    <label for="tab-1">Already Member? Login</label>
+                                </div>
+                            </form>
 							</div>
 						</div>
 					</div>   
