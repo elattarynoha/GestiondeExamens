8d13eb1a0c79edecfc23752337e4dc08a7e43993
MAZIHBouchra <mazihbouchra@gmail.com>
2025-01-09 12:02:46 +0100

    ajout de l'interface modules

---
+++ app/Controllers/ProfController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controllers;
+
+class ProfController extends BaseController{
+    
+    // Récupérer les modules associés au prof:
+    public function getModulesProf(){
+    // Session du professeur connecté:
+    $prof_ID= session()->get('user')['UserID'];
+    $profModel = new ProfModel();
+    $LesModules = $profModel->getModulesByProf($prof_ID);
+    // Passer les modules en view:
+    return view('prof_dashboard',['modules' => $LesModules]);
+    }
+
+
+}
