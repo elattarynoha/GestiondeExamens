a6a2c7a51f30b28b553e10bee4646f0a34c7ffbb
karimamou <moummadekarima@gmail.com>
2025-01-09 21:11:12 +0000

    modification dashboard

--- app/Views/prof_dashboard.php
+++ app/Views/prof_dashboard.php
@@ -1,11 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Prof_Dashboard</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Menu Sidebar</title>
+  <link rel="stylesheet" href="assets/css/dash-prof.css">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+
 </head>
 <body>
-    <h1>Hello Prof!</h1>
+  <div class="sidebar">
+    <div class="sidebar-header">
+      <h2>MyAcademicExams </h2>
+    </div>
+
+    <!-- Section Menu -->
+    <p class="menu-section-title">MENU</p>
+
+    <ul class="menu">
+      <li><a href="#" class="active"><span><i class="fa-solid fa-chart-line"></i></span> Dashboard</a></li>
+      <li><a href="#"><span><i class="fa-regular fa-user"></i></span> Profile</a></li>
+      <li><a href="#"><span><i class="fa-solid fa-book"></i></span> Module</a></li>
+    </ul>
+  </div>
 </body>
 </html>
