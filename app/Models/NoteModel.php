dcfb712d05a694c80eed54b16c6dc09c73e9b40a
Salma babahdi <babahdisalma@gmail.com>
2025-01-13 23:56:29 +0100

    Ajout de table notes & et studentModel pour affichage des notes

---
+++ app/Models/NoteModel.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Models;
+
+use CodeIgniter\Model;
+use App\Entities\Notes;      // Entité Notes
+use App\Models\StudentModel; // Modèle pour gérer les étudiants
+
+class NoteModel extends Model
+{
+    
+}
